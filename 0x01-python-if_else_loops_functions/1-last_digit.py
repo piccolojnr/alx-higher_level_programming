c518b661cdc7302e1ccee708ef3a993e350fd6ec
piccolojnr <rahimdaud246@gmail.com>
2023-10-03 12:42:28 +0000

    last dit zero error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,17 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = (abs(number) % 10) * (number // abs(number))
-if last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
-elif last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-elif last_digit < 6:
-    print(
-        "Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit)
-    )
+if number == 0:
+    print("Last digit of {} is {} and is 0".format(number, number))
+else:
+    last_digit = (abs(number) % 10) * (number // abs(number))
+    if last_digit == 0:
+        print("Last digit of {} is {} and is 0".format(number, last_digit))
+    elif last_digit > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    elif last_digit < 6:
+        print(
+            "Last digit of {} is {} and is less than 6 and not 0".format(
+                number, last_digit
+            )
+        )
