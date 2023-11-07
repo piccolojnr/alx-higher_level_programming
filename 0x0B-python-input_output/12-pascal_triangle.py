92701e318983a925e2a93da662ff566d9a4b49a4
piccolojnr <rahimdaud246@gmail.com>
2023-11-07 13:13:46 +0000

    adv

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,23 +6,8 @@
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
-    Args:
-        n (int): The number of rows in the triangle.
-    Returns:
-        A list of lists of integers representing the Pascal's triangle of n.
-        If n <= 0, an empty list is returned.
-        If n == 1, a list with a single 1 is returned.
-        If n > 1, a list with the first n rows of Pascal's triangle is returned.
-        Each row of the triangle is a list of integers representing the values
-        in the row, starting with the value at the top and ending with the value
-        at the bottom. The values in each row are the sum of the values in the
-        row above. The first row of the triangle is always [1].
-        The last row of the triangle is always [1].
-        The values in each row are the sum of the values in the row above.
-        The first row of the triangle is always [1].
-        The last row of the triangle is always [1].
-        The values in each row are the sum of the values in the row above.
+    Returns a list of lists of integers representing the
+    Pascal's triangle of n.
     """
     if n <= 0:
         return []
