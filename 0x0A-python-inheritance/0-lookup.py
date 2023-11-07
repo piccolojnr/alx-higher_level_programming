bc6155dc40f94fc226039135666ebaaee619d07c
piccolojnr <rahimdaud246@gmail.com>
2023-11-07 12:30:23 +0000

    man4

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 """
-    This module contains a function that returns a list of attributes and methods of an object
+    This module contains a function that returns
+      a list of attributes and methods of an object
 """
 
 
 def lookup(obj):
     """
-    functions to return the list of availble attributes and methods of an object
+    functions to return the list of availble
+      attributes and methods of an object
     """
     return dir(obj)
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,10 +20,8 @@ class BaseGeometry:
         """
         Validates value
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
         return value
-
-
