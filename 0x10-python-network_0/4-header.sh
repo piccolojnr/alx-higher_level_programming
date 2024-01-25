31404bc7bc3e4f3ee85c5985822a480d37b35146
piccolojnr <rahimdaud246@gmail.com>
2024-01-25 23:03:28 +0000

    3

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-
-
 curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
